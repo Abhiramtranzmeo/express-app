7834299ae1e37037fc9b4cea5f6219f4405bddac
Abhiramtranzmeo <abhiram.ts@team.tranzmeo.com>
2023-07-27 15:46:22 +0530

    user authentication

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.listen(3000, '0.0.0.0');
 
 
 import userRouter from './api/src/routers/userRouter.js';
-import authRouter from './api/src/routers/authRouter.js';
 
 
 app.use('/user', userRouter);
